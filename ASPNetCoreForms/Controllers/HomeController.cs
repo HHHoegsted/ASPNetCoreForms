b2cbcc90cee2eda01ca700b19ce37c831eea3aab
Hans-Henrik Høgsted <hh.hoegsted@gmail.com>
2019-11-26 12:31:39 +0100

    Add project files.

---
+++ ASPNetCoreForms/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ASPNetCoreForms.Models;
+
+namespace ASPNetCoreForms.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(ProductEditModel model)
+        {
+            string message = "";
+            
+            if (ModelState.IsValid)
+            {
+                message = "Product " + model.Name + " Rate " + model.Rating.ToString() + " with rating " + model.Rate.ToString() + " created successfully";
+            }
+            else
+            {
+                message = "Failed to create product. Please try again";
+            }
+
+            return Content(message);
+        }
+    }
+}
